7cc320294216d99244f34638c524a530b7db9d8b
Thomas-Eble <tje29@pitt.edu>
2026-03-24 11:17:38 -0400

    Hello World blinking LEDs for GitHub Assignment

---
+++ HelloWorld.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(8, LOW);
+  if(digitalRead(5, HIGH)){
+    digitalWrite(8, HIGH);
+  };
+            
+}
