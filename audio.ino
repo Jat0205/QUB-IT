82d881b20c6d8ebc50f2ae6c5f7cf713141fce7c
Jannet Trabelsi <159489080+Jat0205@users.noreply.github.com>
2026-04-22 18:23:17 -0400

    Add files via upload

---
+++ audio.ino
@@ -0,0 +1,80 @@
+#include "Arduino.h"
+#include <LiquidCrystal_I2C.h>
+#include "DFRobotDFPlayerMini.h"
+
+#if (defined(ARDUINO_AVR_UNO) || defined(ESP8266))   // Using a soft serial port
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(/*rx =*/4, /*tx =*/5);
+#define FPSerial softSerial
+#else
+#define FPSerial Serial1
+#endif
+
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+LiquidCrystal_I2C lcd(0x27, 20, 4); // I2C address 0x27, 20 column and 4 rows
+int i = 0;
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+#if (defined ESP32)
+  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/D3, /*tx =*/D2);
+#else
+  FPSerial.begin(9600);
+#endif
+
+  Serial.begin(115200);
+  
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
+    
+    while(true);
+  }  
+  myDFPlayer.setTimeOut(50000); //Set serial communictaion time out 500ms
+  
+  //----Set volume----
+  myDFPlayer.volume(10);  //Set volume value (0~30).
+  
+  //----Set different EQ----
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  
+  //----Mp3 play----
+  /*myDFPlayer.play(1);  //Play the first mp3
+  delay(10000);
+  lcd.setCursor(0, 0);
+  lcd.print("Initialize it!");
+  myDFPlayer.play(2);  //Play the first mp3
+  delay(10000);
+  myDFPlayer.play(3);  //Play the first mp3
+  delay(10000);
+  myDFPlayer.play(4);  //Play the first mp3
+  delay(10000);*/
+  myDFPlayer.play(1);  //Play the first mp3
+  delay(1000);
+}
+
+void loop()
+{
+  myDFPlayer.play(7);  //Play the first mp3
+  lcd.clear();
+  lcd.print(String(7));
+  delay(10000);
+  myDFPlayer.play(10);  //Play the first mp3
+  lcd.clear();
+  lcd.print(String(10));
+  delay(10000);
+  myDFPlayer.play(8);  //Play the first mp3
+  lcd.clear();
+  lcd.print(String(8));
+  delay(10000);
+  myDFPlayer.play(4);  //Play the first mp3
+  lcd.clear();
+  lcd.print(String(4));
+  delay(10000);
+  myDFPlayer.play(2);  //Play the first mp3
+  lcd.clear();
+  lcd.print(String(2));
+  delay(10000);
+}
