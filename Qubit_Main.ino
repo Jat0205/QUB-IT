294a0db3fec67e26295861515f29306ea750eef5
Jannet Trabelsi <159489080+Jat0205@users.noreply.github.com>
2026-04-22 23:20:13 -0400

    Update Qubit_Main.ino

--- Qubit_Main.ino
+++ Qubit_Main.ino
@@ -19,6 +19,8 @@ bool gameRunning = true;
 int score = 0;             //tracks player score
 int currentCommand = 0;    //what player must do (X, Y, etc)
 bool initialized = false;  //whether game has started
+int qubitState = 0; //current state of the qubit
+
 
 enum GamePhase { WAITING, INITIALIZE, GATE, MEASURE };
 GamePhase currentPhase = INITIALIZE;
@@ -37,10 +39,28 @@ int lastStateY = LOW;  //used for edge detection for Y encoder
 int lastStateZ = LOW;  //used for edge detection for Z encoder
 String COM = "";
 int x, y, z = 0;
+const int NUM_OPS = 8;
+const int NUM_STATES = 6;
+const int state_transition[NUM_OPS][NUM_STATES] = {
+ {0, 0, 0, 0, 0, 0},  // INIT: always |0>
+ {1, 0, 2, 3, 5, 4},  // X: swap 0↔1, swap 4↔5, keep 2,3
+ {1, 0, 3, 2, 4, 5},  // Y: swap 0↔1, swap 2↔3, keep 4,5
+ {0, 1, 3, 2, 5, 4},  // Z: keep 0,1, swap 2↔3, swap 4↔5
+ {2, 3, 0, 1, 5, 4},  // H: swap 0↔2, swap 1↔3, swap 4↔5
+ {-1, -1, 2, 3, -1, -1},  // XMEAS: every -1 needs sep. function to collapse to 2 or 3
+ {-2, -2, -2, -2, 4, 5},  // YMEAS: every -2 needs sep. function to collapse to 4 or 5
+ {0, 1, -3, -3, -3, -3},  // ZMEAS: every -3 needs sep. function to collapse to 1 or 0
+};
+
+
+bool LEDS[NUM_STATES] = {false, false, false, false, false, false};
+bool INIT_LED = false; //LED on the controller that flashes whn turned on and held on when qubit is initialized
+int coin = 0;
+
 
 // ================= PINS ===================
 const int ROTENCX = 2;
-const int ROTENCY = 4;
+const int ROTENCY = 0;
 const int ROTENCZ = 7;
 const int DEBUGLED = 8;
 const int PHOTODIODE = A3;
@@ -49,7 +69,7 @@ const int MEASUREY = A1;
 const int MEASUREZ = A0;
 const int LEDpZ = 3;
 const int LEDmZ = 9;
-const int LEDpX = 5;
+const int LEDpX = 1;
 const int LEDmX = 6;
 const int LEDpY = 10;
 const int LEDmY = 11;
@@ -76,7 +96,7 @@ void setup() {
   FPSerial.begin(9600);
 #endif
 
-  Serial.begin(115200);
+  //Serial.begin(115200);
   
   if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
     
@@ -105,6 +125,12 @@ void setup() {
   pinMode(LEDmX, OUTPUT);
   pinMode(LEDpY, OUTPUT);
   pinMode(LEDmY, OUTPUT);
+  analogWrite(LEDpZ, 0);
+  analogWrite(LEDmZ, 0);
+  digitalWrite(LEDpX, LOW);
+  analogWrite(LEDmX, 0);
+  analogWrite(LEDpY, 0);
+  analogWrite(LEDmY, 0);
 
   lastStateX = digitalRead(ROTENCX);
   lastStateY = digitalRead(ROTENCY);
@@ -138,14 +164,39 @@ void MEASURE_IT() {
   bool result;
   int button_detected = 0;
   result = DETECT_MEASURE(button_detected);
-  
-  if (result == false) {
+  if (result == true){
+
+  if(button_detected == MEASUREX){
+    qubitState = state_transition[5][qubitState];
+    qubitState = collapse_it(qubitState);
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+  }
+  else if (button_detected == MEASUREY){
+    int measureY = digitalRead(A1);
+  if(measureY == HIGH){
+    qubitState = state_transition[6][qubitState];
+    qubitState = collapse_it(qubitState);
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+  }
+  }
+  else if (button_detected == MEASUREZ){
+    int measureZ = digitalRead(A0);
+  if(measureZ == HIGH){
+    qubitState = state_transition[7][qubitState];
+    qubitState = collapse_it(qubitState);
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+  }
+  }
+  recordSuccess();
+  }
+  else{
     recordFailure("4");
-  } else {
-    measure_selection(button_detected);
-    analogWrite(CURRENTSTATE, 255);
-    LED_ON = true;
-    recordSuccess();
   }
 }
 
@@ -266,59 +317,93 @@ void detect_rotation(){
 
 // ================= DETECT GATE =================
 bool DETECT_GATE(int ENCODER_VAL) {
-  int current = digitalRead(ENCODER_VAL);
-  int encoder_mistake;
-  wait_duration = 0;
-  int n = 0;
-  int wrong = 0;
-  
-  int mx, my, mz;
-  int currentStateX = digitalRead(2);
-  int currentStateY = digitalRead(0);
-  int currentStateZ = digitalRead(7);
-  
-  while (n < 6 && wait_duration < duration) {
-    detect_rotation();
-    if (ENCODER_VAL == ROTENCX){
-      n = x;
-      if (y>z){
-        wrong = y;
-      }
-      else{
-        wrong = z;
-      }
-    }
-    else if (ENCODER_VAL == ROTENCY){
-      n = y;
-      if (x>z){
-        wrong = x;
-      }
-      else{
-        wrong = z;
-      }
-    }
-    else if (ENCODER_VAL == ROTENCZ){
-      n = z;
-      if (y>x){
-        wrong = y;
-      }
-      else{
-        wrong = x;
-      }
-    }
-    
-    detect_mx_my_mz(mx, my, mz);
-    int sensorValue = analogRead(PHOTODIODE);    
-    if (mx == HIGH || my == HIGH || mz == HIGH || sensorValue >= 512 || wrong>5) {
-      return false;
-    }
-    
+  if (ENCODER_VAL == ROTENCX){
+    int measureX = digitalRead(A2);
+  if(measureX == HIGH){
+    qubitState = state_transition[5][qubitState];
+    qubitState = collapse_it(qubitState);
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
   }
-  if (n >= 6){
-    return true;
+  }
+  else if (ENCODER_VAL == ROTENCY){
+    int measureY = digitalRead(A1);
+  if(measureY == HIGH){
+    qubitState = state_transition[6][qubitState];
+    qubitState = collapse_it(qubitState);
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+  }
   }
   else{
-    return false;
+int measureZ = digitalRead(A0);
+  if(measureZ == HIGH){
+    qubitState = state_transition[7][qubitState];
+    qubitState = collapse_it(qubitState);
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+  }
+  }
+}
+
+int collapse_it(int qubitState){
+  coin = TCNT0 & 1;
+  if(qubitState == -1){
+    if(coin == 0){
+      return 2;
+    }
+    else{
+      return 3;
+    }
+  }
+  if(qubitState == -2){
+    if(coin == 0){
+      return 4;
+    }
+    else{
+      return 5;
+    }
+  }
+  if(qubitState == -3){
+    if(coin == 0){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
+  return qubitState;
+}
+
+void diode_output(int qubitState){
+  digitalWrite(3, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  switch (qubitState){
+    case 0:
+      digitalWrite(3, HIGH);
+      break;
+    case 1:
+      digitalWrite(9, HIGH);
+      break;
+    case 2:
+      digitalWrite(1, HIGH);
+      break;
+    case 3:
+      digitalWrite(6, HIGH);
+      break;
+    case 4:
+      digitalWrite(10, HIGH);
+      break;
+    case 5:
+      digitalWrite(11, HIGH);
+      break;
   }
 }
 
@@ -357,7 +442,13 @@ bool DETECT_PHOTODIODE() {
   if (sensorValue >= 512){
     initialized = true;
     if (LED_ON) {
+      if (CURRENTSTATE == LEDpX){
+      digitalWrite(LEDpX, LOW);
+      LED_ON = false;
+    }
+    else{
       analogWrite(CURRENTSTATE, 0);
+      LED_ON = false;}
     }
     CURRENTSTATE = LEDpZ;
     analogWrite(CURRENTSTATE, 255);
@@ -414,20 +505,34 @@ bool DETECT_MEASURE(int &button_detected) {
 void DIM_LED() {
   if (LED_ON) {
     if (dim_value > 25) {
-      dim_value -= 25;
+      if (CURRENTSTATE != LEDpX){
+        dim_value -= 25;
       analogWrite(CURRENTSTATE, dim_value);
-    } else {
+    }
+    } else { 
+    if (CURRENTSTATE != LEDpX){
       dim_value = 0;
       analogWrite(CURRENTSTATE, dim_value);
       LED_ON = false;
     }
-  }
-}
+    else{
+      digitalWrite(CURRENTSTATE, LOW);
+      LED_ON = false;}
+    }
+  }}
 
 // ================= MEASURE SELECTION =================
 void measure_selection(int button_detected) {
   if (LED_ON) {
-    analogWrite(CURRENTSTATE, 0);
+    if (CURRENTSTATE != LEDpX){
+      dim_value = 0;
+      analogWrite(CURRENTSTATE, dim_value);
+      LED_ON = false;
+    }
+    else{
+      digitalWrite(CURRENTSTATE, LOW);
+      LED_ON = false;}
+
   }
   
   int current_switch[2];
@@ -456,7 +561,15 @@ void measure_selection(int button_detected) {
   }
   
   // Turn the new LED on
-  analogWrite(CURRENTSTATE, 255);
+  if (CURRENTSTATE != LEDpX){
+      dim_value = 255;
+      analogWrite(CURRENTSTATE, dim_value);
+      LED_ON = true;
+    }
+    else{
+      digitalWrite(CURRENTSTATE, HIGH);
+      LED_ON = true;}
+  
   LED_ON = true;
 }
 
@@ -528,7 +641,14 @@ void switch_selection(int ENCODER_VAL) {
     }
   }
   
-  analogWrite(CURRENTSTATE, 255);
+  if (CURRENTSTATE != LEDpX){
+      dim_value = 255;
+      analogWrite(CURRENTSTATE, dim_value);
+
+    }
+    else{
+      digitalWrite(CURRENTSTATE, HIGH);
+}
   LED_ON = true;
 }
 
